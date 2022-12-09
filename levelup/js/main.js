33b88c9bf8e8c0ba05936ce49e132fc52b35c3bb
unknown <lyubov1996gasanova@gmail.com>
2022-12-09 22:41:24 +0300

    update

--- levelup/js/main.js
+++ levelup/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   var Update = function () {
     $(".seconds").each(function () {
       var count = parseInt($(this.html()));
-      if (count !== 0) {
+      if (count > 0) {
         $(this).html(count - 1);
       } else {
         dec_min();
@@ -19,3 +19,18 @@ $(document).ready(function () {
   };
   setInterval(Update, 1000);
 });
+
+var _Seconds = $(".seconds").text(),
+  int;
+
+int = setInterval(function () {
+  // запускаем интервал
+  if (_Seconds !== 0) {
+    _Seconds--; // вычитаем 1
+    $(".seconds").text(_Seconds); // выводим получившееся значение в блок
+  } else {
+    clearInterval(int); // очищаем интервал, чтобы он не продолжал работу при _Seconds = 0
+    dec_min();
+    setInterval(int);
+  }
+}, 1000);
